b39122a05b9ce64e23bcf2929f88430206e982ca
Vladimir Yashin <vdyashin@users.noreply.github.com>
2018-06-04 20:44:33 +0300

    Simplified and commented the code. Tiny fixes

--- code/functions/kernels.R
+++ code/functions/kernels.R
@@ -1,7 +1,20 @@
 Kh <- function(x, h){
+    # Calculates Gaussian kernel for given x and bandwidths h.
+    # Args:
+    #   x: Array. Values
+    #   h: Array (of the same shape as x). Bandwidths
+    # Returns:
+    #   Array (of the same shape as x). Values of the Gaussian kernel.
     return(dnorm(x/h)/h)
 }
 
 Kddh <- function(x, h){
+    # Calculates second order derivative for Gaussian kernel 
+    # for given x and bandwidths h.
+    # Args:
+    #   x: Array. Values
+    #   h: Array (of the same shape as x). Bandwidths
+    # Returns:
+    #   Array (of the same shape as x). Second order derivative for Gaussian kernel.
     return(((x/h)^2 - 1)*dnorm(x/h)/h^3)
 }
